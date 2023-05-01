da007f7d4c477938d9f38f59570a000dc3629832
Sagnik004 <sagnikchakraborty910@yahoo.com>
2023-05-01 20:07:44 +0530

    react-router-6-fcc added shared layout for top nav

--- react-router-v6-fcc/src/App.jsx
+++ react-router-v6-fcc/src/App.jsx
@@ -1,27 +1,21 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import VansList from './pages/VansList';
 import VanDetail from './pages/VanDetail';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <>
-    <header>
-      <Link to="/">#VANLIFE</Link>
-      <nav>
-        <Link to='/about'>About</Link>
-        <Link to='/vans'>Vans</Link>
-      </nav>
-    </header>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/vans" element={<VansList />} />
-      <Route path="/vans/:id" element={<VanDetail />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<VansList />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Route>
     </Routes>
-    </>
   );
 };
 
