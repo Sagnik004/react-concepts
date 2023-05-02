ec7198d252d8c37d776d098b2708494c4f384ba0
Sagnik004 <sagnikchakraborty910@yahoo.com>
2023-05-03 02:50:44 +0530

    react-router-6-fcc vans filter feature complete

--- react-router-v6-fcc/src/pages/Vans/VansList.jsx
+++ react-router-v6-fcc/src/pages/Vans/VansList.jsx
@@ -1,26 +1,38 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const VansList = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const typeFilter = searchParams.get('type');
 
   useEffect(() => {
     const fetchVans = async () => {
       try {
         const res = await fetch('/api/vans');
         const data = await res.json();
-
         setVans(data.vans);
       } catch (error) {
         console.log(error);
         setVans([]);
       }
+      setIsLoading(false);
     };
 
     fetchVans();
   }, []);
 
-  const vanElements = vans.map((van) => {
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type.toLowerCase() === typeFilter.toLowerCase())
+    : vans;
+
+  const vanElements = displayedVans.map((van) => {
     return (
       <div key={van.id} className="van-tile">
         <Link to={`${van.id}`}>
@@ -40,9 +52,69 @@ const VansList = () => {
     );
   });
 
+  // If there were multiple search params, to retain the other ones we need to use
+  // the below solution (Note: it will remove all search params with the key,
+  // example: ?name=bob&type=rugged&type=simple and removing type will make it as
+  // ?name=bob)
+  function handleFilterChange(key, value) {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
+
+  // Use this function if we want to use Link and native URLSearchParams
+  function genNewSearchParamString(key, value) {
+    const sp = new URLSearchParams(searchParams);
+    if (value === null) {
+      sp.delete(key);
+    } else {
+      sp.set(key, value);
+    }
+    return `?${sp.toString()}`;
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          className={`van-type simple ${
+            typeFilter?.toLowerCase() === 'simple' ? 'selected' : ''
+          }`}
+          onClick={() => setSearchParams({ type: 'simple' })}
+        >
+          Simple
+        </button>
+        <button
+          className={`van-type luxury ${
+            typeFilter?.toLowerCase() === 'luxury' ? 'selected' : ''
+          }`}
+          onClick={() => setSearchParams({ type: 'luxury' })}
+        >
+          Luxury
+        </button>
+        <button
+          className={`van-type rugged ${
+            typeFilter?.toLowerCase() === 'rugged' ? 'selected' : ''
+          }`}
+          onClick={() => setSearchParams({ type: 'rugged' })}
+        >
+          Rugged
+        </button>
+        {typeFilter && (
+          <button
+            className="van-type clear-filters"
+            onClick={() => setSearchParams({})}
+          >
+            Clear filter
+          </button>
+        )}
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
