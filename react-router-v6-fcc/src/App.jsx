726b591b75e0732e12196026c2b29a4cbafd7d9d
Sagnik004 <sagnikchakraborty910@yahoo.com>
2023-05-04 01:43:05 +0530

    react-router-6-fcc 404 page added

--- react-router-v6-fcc/src/App.jsx
+++ react-router-v6-fcc/src/App.jsx
@@ -4,16 +4,17 @@ import Home from './pages/Home';
 import About from './pages/About';
 import VansList from './pages/Vans/VansList';
 import VanDetail from './pages/Vans/VanDetail';
-import Layout from './components/Layout';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
-import HostLayout from './components/HostLayout';
 import HostVans from './pages/Host/HostVans';
 import HostVanDetails from './pages/Host/HostVanDetails';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPricing from './pages/Host/HostVanPricing';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
+import PageNotFound from './pages/404';
+import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
 
 const App = () => {
   return (
@@ -36,7 +37,10 @@ const App = () => {
           </Route>
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        
+
+        {/* 404 page */}
+        <Route path='*' element={<PageNotFound />} />
+
       </Route>
     </Routes>
   );
