39c5fa64c816ecca6d3b04bc77ffa292a0b7abcc
Sagnik004 <sagnikchakraborty910@yahoo.com>
2023-05-14 20:17:21 +0530

    react-router-6-fcc added some firebase firestore features

--- react-router-v6-fcc/src/api.js
+++ react-router-v6-fcc/src/api.js
@@ -1,24 +1,50 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+import { initializeApp } from 'firebase/app';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from 'firebase/firestore/lite';
 
-async function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+// Configure dotenv
+// dotenv.config();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.FIRESTORE_API_KEY,
+  authDomain: import.meta.env.FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIRESTORE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIRESTORE_APP_ID,
+};
+
+// Initialize Firebase, and create db connection
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, 'vans');
 
 export async function getAllVans() {
   try {
-    await sleep(3000);
-    const { data } = await axios.get('/api/vans');
+    const querySnapshot = await getDocs(vansCollectionRef);
+    const vansArr = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
     return {
       isSuccess: true,
-      vans: data.vans,
+      vans: vansArr,
     };
-  } catch (error) {
+  } catch (err) {
+    console.error(err);
     throw new Error({
       message: 'Failed to fetch vans!',
-      status: res.status,
-      statusText: res.statusText,
       isSuccess: false,
     });
   }
@@ -26,56 +52,47 @@ export async function getAllVans() {
 
 export async function getVan(id) {
   try {
-    await sleep(3000);
-    const { data } = await axios.get(`/api/vans/${id}`);
+    const docRef = doc(db, 'vans', id);
+    const vanSnapshot = await getDoc(docRef);
+    const vanData = {
+      ...vanSnapshot.data(),
+      id: vanSnapshot.id,
+    };
     return {
       isSuccess: true,
-      van: data.vans,
+      van: vanData,
     };
   } catch (error) {
     throw new Error({
       message: 'Failed to fetch vans!',
-      status: res.status,
-      statusText: res.statusText,
       isSuccess: false,
     });
   }
 }
 
 export async function getHostsAllVans() {
+  const hostVansQuery = query(vansCollectionRef, where('hostId', '==', '123'));
+
   try {
-    await sleep(3000);
-    const { data } = await axios.get('/api/host/vans');
+    const hostVansSnapshot = await getDocs(hostVansQuery);
+    const hostVansArr = hostVansSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
     return {
       isSuccess: true,
-      vans: data.vans,
+      vans: hostVansArr,
     };
   } catch (error) {
     throw new Error({
       message: 'Failed to fetch host vans!',
-      status: res.status,
-      statusText: res.statusText,
       isSuccess: false,
     });
   }
 }
 
 export async function getHostsVan(id) {
-  try {
-    await sleep(3000);
-    const { data } = await axios.get(`/api/host/vans/${id}`);
-    return {
-      isSuccess: true,
-      van: data.vans,
-    };
-  } catch (error) {
-    throw new Error({
-      message: 'Failed to fetch vans!',
-      status: res.status,
-      statusText: res.statusText,
-      isSuccess: false,
-    });
-  }
+  return getVan(id);
 }
 
 export async function loginUser(creds) {
